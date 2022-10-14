ff4601ebb04f65a5be20066900a9c593810f2b08
Artem <93729278+Nikolaev3Artem@users.noreply.github.com>
2022-10-14 11:38:34 +0300

    added func to open inventory

--- Assets/ScriptableObject/Sources/Inventory.cs
+++ Assets/ScriptableObject/Sources/Inventory.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     public Action<Item> onItemAdded;
     [SerializeField] List<Item> StartItems = new List<Item>();
 
+    public bool TF = true;
+
+    public GameObject obj;
     public List<Item> InventoryItems { get; private set; }
 
 
@@ -21,10 +25,32 @@ public class Inventory : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        OpenCloseInv();
+    }
+
 
     public void AddItem(Item item)
     {
         InventoryItems.Add(item);
         onItemAdded?.Invoke(item);
+    }
+
+    public void OpenCloseInv()
+    {
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            if (TF == true)
+            {
+                obj.SetActive(false);
+                TF = false;
+            }
+            else if(TF == false)
+            {
+                obj.SetActive(true);
+                TF = true;
+            }
+        }
     }
 }
