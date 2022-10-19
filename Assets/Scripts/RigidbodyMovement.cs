c1cc5db471f11daf37ec7b8d67e4564d2955b68b
VovaGatova <demkov.v.e@gmail.com>
2022-10-19 14:17:54 +0300

    исправлена Анимация
    Я исправил анимацию Бега
    Так же я сделал так чтобы в будущем можно было бы добавить анимацию бега назад вправо влево

--- Assets/Scripts/RigidbodyMovement.cs
+++ Assets/Scripts/RigidbodyMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class RigidbodyMovement : BaseCharacterMovement
 {
+    // Прости почти все переписал)
     private Animator animator;
     private new Rigidbody rigidbody;
     private void Start()
@@ -10,10 +11,31 @@ public class RigidbodyMovement : BaseCharacterMovement
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
     }
+    
     private void FixedUpdate()
     {
+        
         base.Update();
         rigidbody.MovePosition(transform.position + movementVector * movementSpeed * Time.fixedDeltaTime);
-        animator.SetFloat("speed", Vector3.ClampMagnitude(movementVector, 1).magnitude);
+        //animator.SetFloat("speed", Vector3.ClampMagnitude(movementVector, 1).magnitude);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (v > 0){ // Так надо чтобы мы смогли сделать чтобы у персонажа были анимации во все стороны :3
+            animator.SetBool("Run", true);
+        }
+        if (v < 0){ 
+            animator.SetBool("Run", true);
+        }
+        if (v == 0){
+            if (h == 0){
+            animator.SetBool("Run", false);
+            }
+        }
+        if (h > 0){ 
+            animator.SetBool("Run", true);
+        }
+        if (h < 0){ 
+            animator.SetBool("Run", true);
+        }
     }
 }
